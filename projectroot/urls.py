0b93d0d2bbc9ebdc3d17af1dc04670ea7493db92
Ryan Jacques <ryanj@maxxpotential.com>
2018-11-24 12:28:38 -0500

    The inconsistency in spaces and tabs bothered me so I fixed it

--- projectroot/urls.py
+++ projectroot/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
 	path('', include('directoryapp.urls')),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+	path('admin/', admin.site.urls),
+	path('api-auth/', include('rest_framework.urls'))
 ]
