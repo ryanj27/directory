a2e906700b96830a7ad8b6b6789cac79d80e4e14
Ryan Jacques <ryanj@maxxpotential.com>
2018-11-24 14:10:36 -0500

    Add Explorer for reporting

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## A simple Django app using Docker Compose
+
+You may need to go inside the web container and run a migrate in order for some of the packages to work properyly. Django Sequel Explorer needs it for sure.
+Ex. `docker exec -it [container ID] /bin/bash` and then `./manage.py migrate`
--- projectroot/urls.py
+++ projectroot/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 urlpatterns = [
 	path('', include('directoryapp.urls')),
 	path('admin/', admin.site.urls),
-	path('api-auth/', include('rest_framework.urls'))
+	path('api-auth/', include('rest_framework.urls')),
+	path('explorer/', include('explorer.urls')),
 ]
 
 if settings.DEBUG:
